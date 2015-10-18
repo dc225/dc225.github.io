cfb112bbe56a023e3e68460f884c80df67abc903
Josh Richard <josh.richard@bitbayou.com>
2015-10-18 01:09:41 -0500

    different angle he says

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,8 +6,15 @@
     url: "/feed.xml",
     dataType: "xml",
     success: function( xml ) {
+      var x = 0;
       $(xml).find("entry").each(function() {
-        $("#blog_feed").append('<a href="' + $(this).find("link").attr("href") + '" class="list-group-item"><h4>' + $(this).find("title").text() + '</h4></a>');
+        if( x < 3 ) {
+          $("#blog_feed").append('<a href="' + $(this).find("link").attr("href") + '" class="list-group-item"><h4>' + $(this).find("title").text() + '</h4></a>');
+          x++;
+        }
+        else {
+          return false;
+        }
       });
     }
   });
