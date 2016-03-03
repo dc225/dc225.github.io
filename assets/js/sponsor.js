cfc3cfc4f9a5b89e212ed4007b36eff7ad75b122
Josh Richard <josh.richard@gmail.com>
2016-03-03 13:58:52 -0600

    Update sponsor.js

--- assets/js/sponsor.js
+++ assets/js/sponsor.js
@@ -1,13 +1,3 @@
 (function($){
-	var sponsors = {
-		mediumRectangle: [
-			
-		]
-	}, r;
-	
-	$(".mediumRectangleSponsor").each(function() {
-		r = Math.floor(Math.random() * sponsors.mediumRectangle.length);
-		$(this).html('<a href="' + sponsors.mediumRectangle[r][1] + '" title="' + sponsors.mediumRectangle[r][2] + '" target="_blank"><img src="' +
-		sponsors.mediumRectangle[r][0] + '" alt="' + sponsors.mediumRectangle[r][2] + '" border="0" /></a>');
-	});
+
 }(jQuery));
