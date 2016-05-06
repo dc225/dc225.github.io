88dfb79f41e550d728ff34011973f87bc7357793
Josh Richard <josh.richard@bitbayou.com>
2016-05-05 21:37:33 -0500

    maybee

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -2,6 +2,17 @@
   "use strict";
 
   /* jshint ignore:start */
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '242215676139537');
+  fbq('track', "PageView");
+  /* jshint ignore:end */
+
+  /* jshint ignore:start */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
