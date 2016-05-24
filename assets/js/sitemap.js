7f49565f78a5fc7ebf4e34a5615a76bdabb277f7
Josh Richard <josh.richard@gmail.com>
2016-05-23 23:32:44 -0500

    what if I put this here

---
+++ assets/js/sitemap.js
@@ -0,0 +1,21 @@
+(function($) {
+  "use strict";
+
+$.ajax({
+  type: "GET",
+  url: "http://defcon225.org/sitemap.xml",
+  dataType: "xml",
+  success: function( xml ) {
+    xml = $(xml).find("loc");
+    var x, y = [];
+      for(x = 0; x < xml.length; x++) { y.push(xml[x].innerHTML); }
+        y.reverse();
+      for(x = 0; x < xml.length; x++) {
+        var url = y[x];
+        $("#sitemap").append('<a href="' + url + '" class="list-group-item">' +
+        '<h4>' + url + '</h4></a>');
+      }
+  }
+});
+
+}(jQuery));
