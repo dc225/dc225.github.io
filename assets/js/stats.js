f4238e3eabdd4d91ac4a793714f9fc775f5c8b20
Josh Richard <josh.richard@gmail.com>
2016-10-07 18:57:37 -0500

    whyyyyy did i just do that

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,12 +1,3 @@
-window._idl = {};
-_idl.variant = "modal";
-(function() {
-    var idl = document.createElement('script');
-    idl.async = true;
-    idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
-    document.getElementsByTagName('body')[0].appendChild(idl);
-})();
-
 (function($) {
   "use strict";
 
