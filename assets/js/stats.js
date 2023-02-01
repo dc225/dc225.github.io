95fb7966e94bf726685d272ca635f9e899d845f7
Josh Richard <josh.richard@gmail.com>
2023-01-31 19:01:43 -0600

    remove missing content

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -2,15 +2,6 @@
   "use strict";
 
   /* jshint ignore:start */
-  window._idl = {};
-  _idl.variant = "modal";
-  (function() {
-      var idl = document.createElement('script');
-      idl.async = true;
-      idl.src = 'https://members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'modal');
-      document.getElementsByTagName('body')[0].appendChild(idl);
-  })();
-
   !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
   n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
